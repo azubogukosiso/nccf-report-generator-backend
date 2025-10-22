0345508f36df5695eebb9a52412553d13b86a748
azuboguko@gmail.com <azuboguko@gmail.com>
2025-10-22 15:17:42 +0100

    feat: implement "edit report" feature

--- src/controllers/reportsController.ts
+++ src/controllers/reportsController.ts
@@ -6,11 +6,30 @@ const viewAllReports = async (req: Request, res: Response) => {
   if (allReports) res.json(allReports).status(200);
 };
 
+const viewSingleReport = async (req: Request, res: Response) => {
+  const reportId = req.params.id;
+  const report = await Report.findById(reportId);
+  if (report) res.json(report).status(200);
+};
+
 const createReports = async (req: Request, res: Response) => {
   const newRecord = await Report.create(req.body);
 
   if (newRecord)
     res.json({ message: "Report created successfully!" }).status(200);
+};
+
+const editReports = async (req: Request, res: Response) => {
+  const reportId = req.query.id;
+
+  if (reportId) {
+    const editedReport = await Report.findByIdAndUpdate(reportId, req.body);
+
+    if (editedReport)
+      res.json({ message: "Report edited successfully!" }).status(201);
+  } else {
+    res.json({ message: "Report not found!" }).status(404);
+  }
 };
 
 const deleteReports = async (req: Request, res: Response) => {
@@ -22,4 +41,10 @@ const deleteReports = async (req: Request, res: Response) => {
     res.json({ message: "Report deleted successfully!" }).status(201);
 };
 
-export { createReports, viewAllReports, deleteReports };
+export {
+  createReports,
+  viewAllReports,
+  viewSingleReport,
+  deleteReports,
+  editReports,
+};
--- src/routes/reports.ts
+++ src/routes/reports.ts
@@ -3,13 +3,17 @@ import express from "express";
 import {
   createReports,
   viewAllReports,
+  viewSingleReport,
   deleteReports,
+  editReports,
 } from "../controllers/reportsController";
 
 const router = express.Router();
 
 router.post("/", createReports);
 router.get("/", viewAllReports);
+router.get("/:id", viewSingleReport);
+router.patch("/", editReports);
 router.delete("/", deleteReports);
 
 export default router;
